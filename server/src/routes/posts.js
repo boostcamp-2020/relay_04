896b7b824b0494493c076f951fd74e92cf1abb58
Chanyeong Cho <cyjo9603@naver.com>
2020-08-07 18:27:01 +0900

    [feat] Set posts api

--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -1,42 +1,27 @@
 const express = require('express');
 
+const Post = require('../../models/post');
+const User = require('../../models/user');
+
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-  let body = req.body;
-  let page = body.page;
+  try {
+    const posts = await Post.findAll({
+      attributes: ['id', 'title', 'content', 'bad'],
+      include: [
+        {
+          model: User,
+          attributes: ['id', 'userid', 'bad'],
+        },
+      ],
+    });
 
-  // db 요청 부분
-  let result = { // db로부터 받은 object;
-    postList : [
-      {
-        id : 1,
-        writer : {
-	        id : 1,
-          userid : "userid",
-          userpw : "userpw",
-          bad : false
-	      },
-        title : "title",
-        content : "content",
-        bad : false
-      },
-      {
-        id : 2,
-        writer : {
-	        id : 1,
-          userid : "userid",
-          userpw : "userpw",
-          bad : false
-	      },
-        title : "title",
-        content : "content",
-        bad : false
-      }
-    ]
+    return res.json(posts);
+  } catch (error) {
+    console.error(error);
+    next(error);
   }
-
-  return res.json(result);
 });
 
 module.exports = router;
